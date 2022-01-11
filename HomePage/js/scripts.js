e160b8a2b670a03930fa6fc71759035154e07dd1
Shamser Bahadur Magar <88875776+shamserpulami@users.noreply.github.com>
2022-01-11 09:04:40 -0500

    upload

--- HomePage/js/scripts.js
+++ HomePage/js/scripts.js
@@ -74,3 +74,16 @@ FB.api(
   }
 );
 
+
+
+
+
+
+
+
+
+        var blink = document.getElementById('blink');
+        setInterval(function() {
+            blink.style.opacity = (blink.style.opacity == 0 ? 1 : 0);
+        }, 1500);
+   
