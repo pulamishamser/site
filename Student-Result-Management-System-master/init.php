c07df190b2ff4166a23d345803f5e8ef67a8e915
Shamser Bahadur Magar <88875776+shamserpulami@users.noreply.github.com>
2022-02-16 09:46:25 -0500

    delete

--- Student-Result-Management-System-master/init.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$conn = mysqli_connect($servername, $username, $password);
-	$db = mysqli_select_db($conn,'srms');
-
-
-?>
