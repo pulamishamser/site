fe0997ceaac71943bfe707f47497d7a837de17a9
Shamser Bahadur Magar <88875776+shamserpulami@users.noreply.github.com>
2022-07-02 21:43:28 +0545

    upload

---
+++ Result/js/sharebtn.js
@@ -0,0 +1,8 @@
+﻿
+
+const shareBtn = document.querySelector('.share-btn');
+const shareOptions = document.querySelector('.share-options');
+
+shareBtn.addEventListener('click', () => {
+    shareOptions.classList.toggle('active');
+})
