d3f8eee1489ff20880f67755cd6b0c5a009aebab
Shamser Bdr Magar <88875776+pulamishamser@users.noreply.github.com>
2023-05-15 10:38:24 +0545

    upload
    Signed-off-by: Shamser Bdr Magar <88875776+pulamishamser@users.noreply.github.com>

---
+++ Quiz_Show/app.js
@@ -0,0 +1,495 @@
+/* Javascript Code For Quiz App*/
+/* Created and coded by Abhilash Narayan */
+/* Quiz source: w3schools.com */
+/* Added Quiz: shamserpulami */
+var quiz = {
+"JS": [
+{
+
+"id":1,
+"question": "नेपालको प्रामाणिक समय कहिले निर्धारण भएको हो ? ",
+"options": [
+{
+"a": "२०४३ बैसाख १",
+"b": "२०४२ बैसाख १",
+"c": "२०३० बैसाख १",
+"d": "२०४५ भाद्र १ "
+}
+],
+"answer": "२०४२ बैसाख १;",
+"score": 0,
+"status": ""
+},
+{
+
+"id": 2,
+"question": "हिमाली जिल्लामा कतिवटा जिल्लाहरु छन् ?",
+"options": [
+{
+"a": "१८",
+"b": "१९",
+"c": "२०",
+"d": "२१",
+}
+],
+"answer": "२१;",
+"score": 0,
+"status": ""
+},
+{
+
+"id": 3,
+"question": "सोलुखुम्बु जिल्लामा पर्ने दुई जिल्लाहरु छुट्टयाउनुहोस् ?",
+"options": [
+{
+"a": "लोत्से र मकालु",
+"b": "मनास्लु र मकालु",
+"c": "चोयु र लोत्से",
+"d": "चोयु र मनास्लु ",
+}
+],
+"answer": "चोयु र लोत्से",
+"score": 0,
+"status": ""
+},
+{
+"id": 4,
+"question": "नेपालको संबिधानको कुन अनुसूचिमा सँघको अधिकारसम्बन्धि व्यवस्था गरेकोछ ?",
+"options": [
+{
+"a": "अनुसूचि ५",
+"b": "अनुसूचि ६",
+"c": "अनुसूचि ७"
+"d": "अनुसूचि ९"
+}
+],
+"answer": "अनुसूचि ५",
+"score": 0,
+"status": ""
+},
+{
+"id": 5,
+"question": "प्रदेश नं ५ को सबैभन्दा सानो जिल्ला कुन हो ?",
+"options": [
+{
+"a": "ताप्लेजुङ",
+"b": "पाँचथर",
+"c": "तेह्रथुम",
+"d": "ईलाम",
+}
+],
+"answer": "तेह्रथुम",
+"score": 0,
+"status": ""
+},
+{
+"id": 6,
+"question": "कसको पालादेखि मालपोत कर उठाउने व्यवस्था गर्न थलियो?",
+"options": [
+{
+"a": "पृथ्वी नारायण शाह",
+"b": "जङ्ग बहादुर राणा",
+"c": "पदम शमसेर",
+"d": "रणोदिप",
+}
+],
+"answer": "रणोदिप",
+"score": 0,
+"status": ""
+},
+{
+"id": 7,
+"question": "२०१५ सालको पहिलो आमचुनाबमा कतिबर्ष उमेर पुगेकालाई भोट हाल्ने अधिकार थियो ?",
+"options": [
+{
+"a": "१८",
+"b": "१९",
+"c": "२०",
+"d": "२१",
+}
+],
+"answer": "myFunction()",
+"score": 0,
+"status": ""
+},
+{
+"id": 8,
+"question": "जनमत संग्रहको चुनाब कहिले भएको थियो ?",
+"options": [
+{
+"a": "२०३६ जेष्ठ १०",
+"b": "२०३७ बैसाख १०",
+"c": "२०३७ बैसाख २०",
+"d": "२०३६ जेष्ठ २०",
+}
+],
+"answer": "२०३७ बैसाख १",
+"score": 0,
+"status": ""
+},
+{
+"id": 9,
+"question": "दोश्रो आम निर्वाचन २०४८मा नेपाली काँग्रेशले कति शिट जितेको थियो?",
+"options": [
+{
+"a": "१०९",
+"b": "११०",
+"c": "१११",
+"d": "११२."
+}
+],
+"answer": "११०",
+"score": 0,
+"status": ""
+},
+{
+"id": 10,
+"question": "माओवादीको जनआन्दोलनका कारणले नेपालमा पहिलो पटक कहिले संकटकाल घोषणा भएको थियो ?",
+"options": [
+{
+"a": "२०५१ फाल्गुन १ ",
+"b": "२०५७ चैत्र १ १",
+"c": "२०५७ मंसिर ११",
+"d": "२०५९ जेष्ठ ११",
+}
+],
+"answer": "२०५७ मंसिर ११",
+"score": 0,
+"status": ""
+},
+{
+
+
+"id": 11,
+"question": "नेपालको प्रथम मुख्तियारको नाम के हो ?",
+"options": [
+{
+"a": "फत्तेजङ्ग शाह ",
+"b": "भिमसेन थापा",
+"c": "पं. रङ्गनाथ पौडेल",
+"d": "पुष्कर शाह ",
+}
+],
+"answer": "भिमसेन थापा",
+"score": 0,
+"status": ""
+},
+{
+
+
+"id": 12,
+"question": "तालको पानी बाहिर निकाल्न साइफन जडान गरिएको तालको नाम केहो ?",
+"options": [
+{
+"a": "छ्योरोल्पा ताल",
+"b": "शेफोक्सुण्डो ताल",
+"c": "रारा ताल",
+"d": "कुनै पनि हैन",
+}
+],
+"answer": "छ्योरोल्पा ताल",
+"score": 0,
+"status": ""
+},
+{
+"id": 13,
+"question": "किँरात राज्यका संस्थापक को हुन् ?",
+"options": [
+{
+"a": "यलम्बर",
+"b": "गास्ती",
+"c": "जितेदास्ती",
+"d": "हुमति",
+
+}
+],
+"answer": "यलम्बर",
+"score": 0,
+"status": ""
+},
+{
+"id": 14,
+"question": "२०७८ को जनगणना अनुसार बार्षिक जनसंख्या बृद्धिदर कति छ ?",
+"options": [
+{
+"a": "0.65%",
+"b": "0.95%",
+"c": "0.85%",
+"d": "0.97%",
+}
+],
+"answer": "0.95%",
+"score": 0,
+"status": ""
+},
+{
+"id": 15,
+"question": "डोयराज्यलाई के नामले चिनिन्छ ?",
+"options": [
+{
+"a": "तिरहुते",
+"b": "खस रज्य",
+"c": "बाइसे राज्य",
+"d": "मल्ल राज्य",
+}
+],
+"answer": "तिरहुते",
+"score": 0,
+"status": ""
+},
+{
+"id": 16,
+"question": "भक्तपुरमा विस्केट जात्रा सुरु गर्ने राजा को हुन्?",
+"options": [
+{
+"a": "जयस्थिति मल्ल",
+"b": "गुणकामदेव",
+"c": "जयप्रकास मल्ल",
+"d": "जगज्योति मल्ल",
+}
+],
+"answer": "जगज्योति मल्ल",
+"score": 0,
+"status": ""
+},
+{
+"id": 17,
+"question": "नेपालको नयाँ नक्सा कहिले जारी भएको थियो ?",
+"options": [
+{
+"a": "मिति २०७७ जेठ ५",
+"b": "मिति २०७८ जेठ ५",
+"c": "मिति २०७७ जेठ २",
+"d": "मिति २०७८ जेठ २",
+}
+],
+"answer": "मिति २०७७ जेठ ५",
+"score": 0,
+"status": ""
+},
+{
+"id": 18,
+"question": "गोरखामा शाहवंशीय राज्यको  स्थापना कहिले भएको थियो ?",
+"options": [
+{
+"a": "वि.सं.१६१६ भाद्र २५ गते",
+"b": "वि.सं.१६१७ भाद्र २६ गते",
+"c": "वि.सं.१६१७ असोज २५ गते",
+"d": "वि.सं.१६१८ असोज २६ गते",
+],
+"answer": "वि.सं.१६१६ भाद्र २५ गते",
+"score": 0,
+"status": ""
+},
+{
+"id": 19,
+"question": "पहिलो बैङ्क (नेपाल बैङ्क) स्थापना कसको पालामा भएको थियो ?",
+"options": [
+{
+"a": "जुद्ध समसेर  ",             
+"b": "पदम समसेर ", 
+"c": "चन्द्र  समसेर ", 
+"d": "मोहन समसेर",
+
+
+}
+],
+"answer": "जुद्ध समसेर ",
+"score": 0,
+"status": ""
+},
+{
+"id": 20,
+"question": "सङ्घको अधिकारको सूची अनुसार कतिवटा अधिकार रहेकाछन् ?",
+"options": [
+{
+"a":  "३३",
+"b":  "३४",
+"c": "३५",
+"d": "३६",
+}
+],
+"answer":  "३५",
+"score": 0,
+"status": ""
+},
+{
+"id": 21,
+"question": "लिच्छवीकालमा जमिनको कुत उठाउने निकायलाई के भनिन्थ्यो  ?",
+"options": [
+{
+"a": "माप्चोक",
+"b": "कुथेर अधिकरण",
+"c": "लिंगवल",
+"d": "शोल्ल अधिकरण",
+}
+],
+"answer": "कुथेर अधिकरण",
+"score": 0,
+"status": ""
+},
+{
+"id": 22,
+"question": "राणाकालमा कुमारीचोकले के काम गर्दथ्यो  ?",
+"options": [
+{
+"a": "परराष्ट्र मामिलासम्बन्धी कार्य ",
+"b": "लेखापरीक्षणसम्बन्धी कार्य ",
+"c": "सैनिक प्रशासनसम्बन्धी",
+"d": "सरकारी कर्मचारीको अभिलेख",
+}
+],
+"answer": "लेखापरीक्षणसम्बन्धी कार्य",
+"score": 0,
+"status": ""
+},
+{
+"id": 23,
+"question": "पन्ध्रौं योजनाको अन्तमा अौसत आयु कति पुगेको हुने छ?",
+"options": [
+{
+"a": "६९.७ वर्ष",
+"b": "७१ वर्ष",
+"c": "७२ वर्ष",
+"d": "६७.७वर्ष",
+}
+],
+"answer": "७२ वर्ष",
+"score": 0,
+"status": ""
+},
+{
+"id": 24,
+"question": "थामी जातिलाई कुन व‌ंशको शाखा मानिन्छ ?",
+"options": [
+{
+"a": "किरात वंश",
+"b": "गोपाल वंश ",
+"c": "ठकुरी वंश",
+"d": "कुनै पनि व‌शका हैन",
+}
+],
+"answer": "किरात वंश",
+"score": 0,
+"status": ""
+},
+{
+"id": 25,
+"question": "राउटे जातिहरूको भाषामा अस्थायी रूपमा बस्ने ठाउँलाई  के भनिन्छ ?",
+"options": [
+{
+"a": "थौला",
+"b": " राउटी ",
+"c": " राउटे",
+"d": " भुयार ",
+}
+],
+"answer": "राउटी",
+"score": 0,
+"status": ""
+
+}
+]
+}
+
+
+
+<!----------javascript-------------------------->
+var quizApp = function () {
+this.score = 0;
+this.qno = 1;
+this.currentque = 0;
+var totalque = quiz.JS.length;
+this.displayQuiz = function (cque) {
+this.currentque = cque;
+if (this.currentque < totalque) {
+$("#tque").html(totalque);
+$("#previous").attr("disabled", false);
+$("#next").attr("disabled", false);
+$("#qid").html(quiz.JS[this.currentque].id + '.');
+$("#question").html(quiz.JS[this.currentque].question);
+$("#question-options").html("");
+for (var key in quiz.JS[this.currentque].options[0]) {
+if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
+$("#question-options").append(
+"<div class='form-check option-block'>" +
+"<label class='form-check-label'>" +
+"<input type='radio' class='form-check-input' name='option' id='q" + key + "' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
+quiz.JS[this.currentque].options[0][key] +
+"</span></label>"
+);
+}
+}
+}
+if (this.currentque <= 0) {
+$("#previous").attr("disabled", true);
+}
+if (this.currentque >= totalque) {
+$('#next').attr('disabled', true);
+for (var i = 0; i < totalque; i++) {
+this.score = this.score + quiz.JS[i].score;
+}
+return this.showResult(this.score);
+}
+}
+this.showResult = function (scr) {
+$("#result").addClass('result');
+$("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr + '/' + totalque + "</h1>");
+for (var j = 0; j < totalque; j++) {
+var res;
+if (quiz.JS[j].score == 0) {
+res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
+} else {
+res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';
+}
+$("#result").append(
+'<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +
+'<div><b>Correct answer is:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
+'<div class="last-row"><b>Score:</b> &nbsp;' + res +
+'</div>'
+);
+}
+}
+this.checkAnswer = function (option) {
+var answer = quiz.JS[this.currentque].answer;
+option = option.replace(/</g, "&lt;") //for <
+option = option.replace(/>/g, "&gt;") //for >
+option = option.replace(/"/g, "&quot;")
+if (option == quiz.JS[this.currentque].answer) {
+if (quiz.JS[this.currentque].score == "") {
+quiz.JS[this.currentque].score = 1;
+quiz.JS[this.currentque].status = "correct";
+}
+} else {
+quiz.JS[this.currentque].status = "wrong";
+}
+}
+this.changeQuestion = function (cque) {
+this.currentque = this.currentque + cque;
+this.displayQuiz(this.currentque);
+}
+}
+var jsq = new quizApp();
+var selectedopt;
+$(document).ready(function () {
+jsq.displayQuiz(0);
+$('#question-options').on('change', 'input[type=radio][name=option]', function (e) {
+//var radio = $(this).find('input:radio');
+$(this).prop("checked", true);
+selectedopt = $(this).val();
+});
+});
+$('#next').click(function (e) {
+e.preventDefault();
+if (selectedopt) {
+jsq.checkAnswer(selectedopt);
+}
+jsq.changeQuestion(1);
+});
+$('#previous').click(function (e) {
+e.preventDefault();
+if (selectedopt) {
+jsq.checkAnswer(selectedopt);
+}
+jsq.changeQuestion(-1);
+});
