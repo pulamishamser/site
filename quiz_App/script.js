8e1f9c9a67f852ae17d8028ddf6c23057eaa08cb
Shamser Bdr Magar <88875776+pulamishamser@users.noreply.github.com>
2023-06-25 21:43:32 +0545

    Update
    Signed-off-by: Shamser Bdr Magar <88875776+pulamishamser@users.noreply.github.com>

--- quiz_App/script.js
+++ quiz_App/script.js
@@ -195,11 +195,11 @@ function startTimer(time){
 }
 
 function startTimerLine(time){
-    counterLine = setInterval(timer, 89);
+    counterLine = setInterval(timer, 79);
     function timer(){
         time += 1; //upgrading time value with 1
         time_line.style.width = time + "px"; //increasing width of time_line with px by time value
-        if(time > 299){ //if time value is greater than 399
+        if(time > 399){ //if time value is greater than 399
             clearInterval(counterLine); //clear counterLine
         }
     }
