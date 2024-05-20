47fc92e14c77c0078f70a2b3f0f7c66ddfe3b60d
Shamser Bdr Magar <88875776+pulamishamser@users.noreply.github.com>
2024-05-20 07:28:13 +0545

    upload
    Signed-off-by: Shamser Bdr Magar <88875776+pulamishamser@users.noreply.github.com>

---
+++ Home/fb.js
@@ -0,0 +1,3 @@
+
+
+document.querySelector("#mount_0_0_9H > div > div:nth-child(1) > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div > div.x78zum5.xdt5ytf.x1t2pt76.x1n2onr6.x1ja2u2z.x10cihs4 > div.x78zum5.xdt5ytf.x1t2pt76 > div > div > div:nth-child(1) > div.x9f619.x1n2onr6.x1ja2u2z.x78zum5.x2lah0s.xl56j7k.x1qjc9v5.xozqiw3.x1q0g3np.x1l90r2v.x1ve1bff > div > div > div.x6s0dn4.x78zum5.xvrxa7q.x9w375v.xxfedj9.x1roke11.x1es02x0 > div.x1ifrov1.x1i1uccp.x1stjdt1.x1yaem6q.x4ckvhe.x2k3zez.xjbssrd.x1ltux0g.xit7rg8.xc9uqle.x17quhge > div > div > div:nth-child(1) > div > div > div > div.x6s0dn4.x78zum5.xl56j7k.x1608yet.xljgi0e.x1e0frkt > div:nth-child(2) > span > span")
