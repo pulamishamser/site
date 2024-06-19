f26ea435387480366a488c2cc534a851798f4acd
Shamser Bdr Magar <88875776+pulamishamser@users.noreply.github.com>
2024-06-19 06:06:13 +0545

    Update
    Signed-off-by: Shamser Bdr Magar <88875776+pulamishamser@users.noreply.github.com>

--- Folio/forms/contact.php
+++ Folio/forms/contact.php
@@ -9,7 +9,7 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'contact@example.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  if( file_exists($php_email_form = 'assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
@@ -18,7 +18,7 @@
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
+  $contact->to = $shamserpulami500@gmail.com;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
