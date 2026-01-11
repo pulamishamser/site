929969ebe92b5df5df07f2bf4767708b713fa2d7
Shamser Bdr Magar <88875776+pulamishamser@users.noreply.github.com>
2026-01-11 14:54:48 +0545

    Update
    Signed-off-by: Shamser Bdr Magar <88875776+pulamishamser@users.noreply.github.com>

--- shamser/script.js
+++ shamser/script.js
@@ -917,3 +917,26 @@ function convertModalToUnicode() {
     
     unicodeResult.textContent = convertedText || "रूपान्तरण परिणाम यहाँ देखिने";
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// Add this check after showing the modal
+setTimeout(() => {
+    const modal = document.getElementById('welcomeModal');
+    const modalInstance = bootstrap.Modal.getInstance(modal);
+    
+    // If modal is not open, try to open it
+    if (!modal.classList.contains('show')) {
+        const welcomeModal = new bootstrap.Modal(modal);
+        welcomeModal.show();
+    }
+}, 100);
